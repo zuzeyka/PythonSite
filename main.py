3fa4473b87a7c584f6e9d0cd7c12bfa1e7751534
zuzeyka <stepan.tarasenko.26@gmail.com>
2025-02-19 19:27:38 +0100

    added data transfer for register and login pages

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 from flask import Flask, render_template, request
 import random, json
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret_key"
 
 DATA_FILE = "products.json"
+USERS_FILE = "users.json"
 
-def load_products():
+def load_data(file_name):
     try:
-        with open(DATA_FILE, "r") as file:
+        with open(file_name, "r") as file:
             return json.load(file)
     except FileNotFoundError:
         return []
     
-def save_products(products):
-    with open(DATA_FILE, "w") as file:
-        json.dump(products, file)
+def save_data(file_name, data):
+    with open(file_name, "w") as file:
+        json.dump(data, file)
 
-products = load_products()
+products = load_data(DATA_FILE)
+users = load_data(USERS_FILE)
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
 
 @app.route('/')
 def main_page():
@@ -45,13 +63,25 @@ def about():
 def random_number():
     return render_template("random.html", number=random.randint(0, 1000))
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form.get('user-login')
+        password = request.form.get('user-password')
+        print(f"Username: {username}, Password: {password}")
     return render_template('login.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        username = request.form.get('user-login')
+        password = request.form.get('user-password')
+        print(f"Username: {username}, Password: {password}")
     return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    pass
 
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit_product(id):
